8e697c5491cfaec889bee0a7f997cf4bf5fdeec9
Bryan CHARLES <bcharles@we-plus.fr>
2025-02-12 11:18:18 +0100

    Maj affichage page contact

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,14 +1,56 @@
 import React from 'react';
+import './ContactUs.css';
 import Footer from './Footer';
 import Header from './Header';
 
 const ContactUs = () => {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>Pour toute question ou demande de partenariat, contactez-nous à contact@notresitebaseball.fr.</p>
-    </div>
-  );
+    return (
+        <div className="contact-container">
+            <Header />
+            <div className="contact-header">
+                <h1>Contact us</h1>
+                <p>
+                    Don't hesitate to contact us for any information about your company or our players.
+                </p>
+            </div>
+
+            {/* Section Formulaire et Informations */}
+            <div className="contact-content">
+                {/* Formulaire */}
+                <form className="contact-form">
+                    <div className="form-group">
+                        <input type="text" placeholder="First name" required />
+                        <input type="text" placeholder="Last name" required />
+                    </div>
+
+                    <div className="form-group">
+                        <input type="email" placeholder="Email address" required />
+                        <input type="tel" placeholder="Phone number" />
+                    </div>
+
+                    <div className="form-group">
+                        <input type="text" placeholder="Company" />
+                    </div>
+
+                    <div className="form-group">
+                        <textarea placeholder="Message" rows="5" required></textarea>
+                    </div>
+
+                    <button type="submit" className="submit-button">Submit</button>
+                </form>
+
+                {/* Informations de contact */}
+                <div className="contact-info">
+                    <h2>Address</h2>
+                    <p>1234 Lorem Ipsum<br />Lorem Ipsum, GA 30308<br />1234567890<br />xyz@companyname.com</p>
+
+                    <h2>Address 2</h2>
+                    <p>1234 Lorem Ipsum<br />Lorem Ipsum, GA 30308<br />1234567890<br />xyz2@companyname.com</p>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default ContactUs;
