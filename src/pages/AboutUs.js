8ed88c6026207bd1b8b62e6dcca576e1edde6767
Bryan CHARLES <bcharles@we-plus.fr>
2025-02-18 13:58:04 +0100

    add about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from './NavBar';
+import './AboutUs.css'; // Assurez-vous d'importer le fichier CSS correspondant
 
 const AboutUs = () => {
     return (
         <div>
             <Navbar />
-            <h1>About Us</h1>
-            <p>Bienvenue sur notre site dédié au baseball en France. Nous nous engageons à promouvoir le sport et les joueurs locaux.</p>
+            <div className="about-section">
+                <div className="content">
+                    <img src="team2Jpg3.jpg" alt="Votre description" className="image-left" />
+                    <div className="text-right">
+                        <h2>À propos de moi</h2>
+                        <p>Je suis l'agent principal des joueurs, dédié à la promotion du baseball en France ainsi qu'au développement du hockey sur glace français et australien, ainsi que d'autres nationalités.</p>
+                    </div>
+                </div>
+                <div className="content">
+                    <img src="team3Jpg3" alt="Walter Tesonne" className="image-right" />
+                    <div className="text-left">
+                        <h2>À propos de Walter Tesonne</h2>
+                        <p>Walter Tesonne, mon assistant, joue un rôle crucial en aidant nos joueurs à atteindre leur plein potentiel grâce à son expertise et son dévouement.</p>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     );
