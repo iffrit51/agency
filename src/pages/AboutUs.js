b231d17fc217c06fbdfe981e880abca0de16138e
Bryan CHARLES <bcharles@we-plus.fr>
2025-02-20 09:59:27 +0100

    Merge about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from './NavBar';
-import './AboutUs.css'; // Assurez-vous d'importer le fichier CSS correspondant
+import './AboutUs.css';
 
 const AboutUs = () => {
     return (
-        <div>
+        <div className="about-container">
             <Navbar />
             <div className="about-section">
                 <div className="content">
-                    <img src="team2Jpg3.jpg" alt="Votre description" className="image-left" />
-                    <div className="text-right">
+                    <div className="about-image">
+                        <img src="/images/team2Jpg3.jpg" alt="Votre description" />
+                    </div>
+                    <div className="about-text">
                         <h2>À propos de moi</h2>
                         <p>Je suis l'agent principal des joueurs, dédié à la promotion du baseball en France ainsi qu'au développement du hockey sur glace français et australien, ainsi que d'autres nationalités.</p>
                     </div>
                 </div>
                 <div className="content">
-                    <img src="team3Jpg3" alt="Walter Tesonne" className="image-right" />
-                    <div className="text-left">
+                    <div className="about-image">
+                        <img src="/images/team3Jpg.jpg" alt="Sa description" />
+                    </div>
+                    <div className="about-text">
                         <h2>À propos de mon assistant</h2>
-                        <p>Mon assistant, joue un rôle crucial en aidant nos joueurs à atteindre leur plein potentiel grâce à son expertise et son dévouement.</p>
+                        <p>Mon assistant joue un rôle crucial en aidant nos joueurs à atteindre leur plein potentiel grâce à son expertise et son dévouement.</p>
                     </div>
                 </div>
             </div>
